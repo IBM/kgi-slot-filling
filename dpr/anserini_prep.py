0b8646423ee1818af030f989059aa285b40a90bb
Michael Glass <mrglass@us.ibm.com>
2021-06-22 09:53:17 -0400

    fix anserini_prep script to produce directory of output

--- dpr/anserini_prep.py
+++ dpr/anserini_prep.py
@@ -2,12 +2,14 @@ from util.line_corpus import jsonl_lines, write_open
 import ujson as json
 import re
 from util.args_help import fill_from_args
+import os
 
 
 class Options:
     def __init__(self):
         self.input = ''
         self.output = ''
+        self.file_count = 8
 
 
 _WHITESPACE = re.compile(r'\s+')
@@ -16,8 +18,11 @@ opts = Options()
 fill_from_args(opts)
 
 # pid, title, text -> id, contents
-with write_open(opts.output) as f:
-    for line in jsonl_lines(opts.input):
-        jobj = json.loads(line)
-        f.write(json.dumps({'id': jobj['pid'],
-                            'contents': _WHITESPACE.sub(' ', jobj['title'])+'\n\n'+jobj['text']})+'\n')
+outfiles = [write_open(os.path.join(opts.output, f'{j}.jsonl')) for j in range(opts.file_count)]
+for line_ndx, line in enumerate(jsonl_lines(opts.input)):
+    jobj = json.loads(line)
+    f = outfiles[line_ndx % len(outfiles)]
+    f.write(json.dumps({'id': jobj['pid'],
+                        'contents': _WHITESPACE.sub(' ', jobj['title'])+'\n\n'+jobj['text']})+'\n')
+for of in outfiles:
+    of.close()
